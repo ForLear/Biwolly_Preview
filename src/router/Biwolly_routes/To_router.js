1f6524fe2195f2d9f09ef54ee1b7d615be6d249c
toast <905986483@qq.com>
2018-08-03 22:42:28 +0800

    save

---
+++ src/router/Biwolly_routes/To_router.js
@@ -0,0 +1,22 @@
+import Home from '@/pages/home'
+
+import Doc from '@/pages/homePage/doc'
+
+const routes = [
+  {
+    path: '/Doc',
+    name: 'Doc',
+    component: Doc,
+  },
+]
+
+const route = {
+  path: '/',
+  name: '',
+  component: Home,
+  children: [],
+}
+
+route.children.push(...routes)
+
+export default route
