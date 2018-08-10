83d9bc375e8b3b1c95a8101a3e129c5128e546a2
Lear <wangt@mflyhorse.com>
2018-08-10 15:55:01 +0800

    save

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,14 @@ module.exports = {
     '@vue/app'
     // ['es2015', { modules: false }]
   ],
-  plugins: [ 
-    /* 减少element-ui的打包体积 */
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk',
-      },
-    ],
-  ],
+  // plugins: [ 
+  //   /* 减少element-ui的打包体积 */
+  //   [
+  //     'component',
+  //     {
+  //       libraryName: 'element-ui',
+  //       styleLibraryName: 'theme-chalk',
+  //     },
+  //   ],
+  // ],
 }
